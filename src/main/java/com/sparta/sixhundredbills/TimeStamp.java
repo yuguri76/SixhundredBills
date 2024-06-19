ad768f8b43a50cdf24df2c7ee8e444ddc88fa542
gyul <ysy9976@naver.com>
2024-06-19 15:45:23 +0900

    git init

---
+++ src/main/java/com/sparta/sixhundredbills/TimeStamp.java
@@ -0,0 +1,36 @@
+package com.sparta.sixhundredbills;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamp {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
---
+++ src/test/java/com/sparta/sixhundredbills/SixhundredBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sixhundredbills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixhundredBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
