ab7b7cff8fcb6691691f51ed528a2ef5891e1880
gminnimk <tvt1245@naver.com>
2024-06-19 20:14:56 +0900

    feat : 사용자 역할을 정의하는 열거형 생성.

---
+++ src/main/java/com/sparta/sixhundredbills/entity/Role.java
@@ -0,0 +1,11 @@
+package com.sparta.sixhundredbills.entity;
+
+
+// 사용자 역할을 정의하는 열거형(enum)
+//  ㄴ Role 열거형을 사용하여 사용자의 역할을 지정할 수 있음.
+
+
+public enum Role {
+    USER, // 일반 사용자 역할을 나타내는 상수
+    ADMIN // 관리자 역할을 나타내는 상수
+}
