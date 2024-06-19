5d2659e18012776053bde3584143edcc4cb9bfb1
gminnimk <tvt1245@naver.com>
2024-06-19 20:35:44 +0900

    feat : 사용자 회원 가입, 로그인 로직 처리 클래스 생성.

---
+++ src/main/java/com/sparta/sixhundredbills/service/UserService.java
@@ -0,0 +1,67 @@
+package com.sparta.sixhundredbills.service;
+
+import com.sparta.sixhundredbills.entity.Role;
+import com.sparta.sixhundredbills.entity.User;
+import com.sparta.sixhundredbills.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+// 사용자 서비스
+
+// 사용자 등록 및 로그인 로직 처리 & DB 상호작용 => 사용자 관리 기능의 클래스
+// @Service 애노테이션을 통해 이 클래스는 Spring의 서비스로 등록 및 IoC 컨테이너에 의해 관리.
+
+
+
+@Service // 서비스로 등록하여 관리
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    // 사용자 등록 메서드
+    public User signupUser(String username, String password, Role role) {
+
+        // 이미 존재하는 username인지 확인
+        if (userRepository.findByUsername(username).isPresent()) {
+            // 이미 존재할 시 예외 처리
+            throw new RuntimeException("사용자가 이미 존재합니다.");
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(password);
+
+        // 사용자 엔티티 생성 및 설정
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encodedPassword);
+        user.setRole(role);
+
+        // 사용자 저장 후 반환
+        return userRepository.save(user);
+    }
+
+    // 사용자 로그인 메서드
+    public User loginUser(String username, String password) {
+        // username으로 사용자 조회
+        User user = userRepository.findByUsername(username)
+                // 사용자가 존재하지 않을 시 예외 처리
+                .orElseThrow(() -> new RuntimeException("사용자 이름이 존재하지 않습니다."));
+
+        // 비밀번호 일치 여부 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            // 비밀번호 일치하지 않을 시 예외 처리
+            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 사용자 반환
+        return user;
+    }
+}
