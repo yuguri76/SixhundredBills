3ae5963ccc569343ba72ba55a9eb5b26e4c607e2
gminnimk <tvt1245@naver.com>
2024-06-20 15:14:21 +0900

    feat: 필드 및 메서드 추가
    ADD
        - userStatus 필드와 관련된 getUserStatus() 메서드 추가 => 사용자 상태 반환
        - refreshToken 관련 필드 & getter/setter 메서드 추가 => Refresh Token을 관리

--- src/main/java/com/sparta/sixhundredbills/auth/entity/User.java
+++ src/main/java/com/sparta/sixhundredbills/auth/entity/User.java
@@ -7,7 +7,8 @@ import lombok.Setter;
 // DB 테이블에 매핑되는 Entity
 // => 사용자의 정보를 저장하는 역할.
 
-@Entity
+
+@Entity // JPA가 관리하는 엔티티 클래스임을 명시
 @Getter
 @Setter
 public class User {
@@ -23,4 +24,36 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role; // 사용자의 역할 (Enum 타입으로 저장)
+
+
+    // => 추가된 필드 및 메서드
+    // userStatus 필드와 관련된 getUserStatus() 메서드 추가 => 사용자 상태 반환
+    // refreshToken 관련 필드 & getter/setter 메서드 추가 => Refresh Token을 관리
+
+
+    @Setter
+    private UserStatusEnum userStatus; // 사용자의 상태를 나타내는 Enum 타입 필드 , EX) 활성 상태, 비활성 상태 등 저장가능
+    private String refreshToken; // JWT의 Refresh Token을 저장하는 필드  => 사용자가 로그인하고 나서 Refresh Token이 발급될 경우 이 필드에 저장!
+
+
+    // 사용자의 상태를 반환하는 메서드 => 필드 값 반환.
+    public UserStatusEnum getUserStatus() {
+        return userStatus;
+    }
+
+    // 사용자의 역할을 반환하는 메서드 추가
+    public String getRole() {
+        return role.name(); // role의 이름을 문자열로 반환
+    }
+
+
+    // Refresh Token 설정 메서드 => 주어진 문자열을 refreshToken 필드에 저장!
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    // Refresh Token 반환 메서드 => 현재 객체의 refreshToken 필드 값을 반환
+    public String getRefreshToken() {
+        return this.refreshToken;
+    }
 }
