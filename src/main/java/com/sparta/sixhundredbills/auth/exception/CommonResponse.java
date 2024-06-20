28883f7bbba75ba611715cfaf747deff948cca54
gminnimk <tvt1245@naver.com>
2024-06-20 23:03:05 +0900

    style: 임시 주석 처리.

---
+++ src/main/java/com/sparta/sixhundredbills/auth/exception/CommonResponse.java
@@ -0,0 +1,15 @@
+//package com.sparta.sixhundredbills.auth.exception;
+//
+//
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Builder
+//public class CommonResponse<T> {
+//    private String message;
+//    private int statusCode;
+//    private T data;
+//}
