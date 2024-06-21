2588f1f41845be5795cbe7e683e0a338487a864b
gyul <ysy9976@naver.com>
2024-06-22 06:19:32 +0900

    fix : Refactor post-related code and implement JWT token handling
    - @AuthenticationPrincipal을 통해 JWT 토큰 인증을 처리하도록 PostController 리팩토링
    - 코드 컨벤션 한 것과 같이, 생성자 패턴을 builder 패턴으로 바꿈.
    - getPosts 메서드에서 일관된 페이지네이션을 위해 @RequestParam 기본값(5) 추가
    - PostService에서 createPost, updatePost, deletePost 메서드에 적절한 jwt 인증 체크 구현
    - 페이지네이션 지원을 위한 PostRepository 업데이트
    - jwt 관련 코드 중 실행에 방해가 되거나 제대로 작동하지 않는 부분들 수정
    
    FIX
        - JwtAuthenticationEntryPoint.java
        - Post.java
        - PostController.java
        - PostRepository.java
        - PostRequestDto.java
        - PostResponseDto.java
        - PostService.java
        - SecurityConfig.java
        - User.java
        - UserDetailsImpl.java
    
    BREAKING CHANGE(중요한 변경 사항): user 엔티티, SecurityConfig, UserDetailsImpl 일부 수정

--- src/main/java/com/sparta/sixhundredbills/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/sixhundredbills/post/dto/PostResponseDto.java
@@ -8,15 +8,14 @@ import java.time.LocalDateTime;
 
 @Getter
 public class PostResponseDto {
-    private Long id; // 게시물 ID
-    private Long userId; // 작성자 ID
-    private String showName; // 익명으로 표시될 이름
-    private String content; // 게시물 내용
-    private String category; // 게시물 카테고리
-    private LocalDateTime createdAt; // 생성일자
-    private LocalDateTime updatedAt; // 수정일자
+    private Long id;
+    private Long userId;
+    private String showName;
+    private String content;
+    private String category;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    // 모든 필드를 포함하는 생성자
     @Builder
     public PostResponseDto(Long id, Long userId, String showName, String content, String category, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
@@ -28,7 +27,10 @@ public class PostResponseDto {
         this.updatedAt = updatedAt;
     }
 
-    // Post 엔티티를 기반으로 PostResponseDto를 생성하는 생성자
+    /**
+     * Post 엔티티를 이용해 PostResponseDto 객체를 생성하는 생성자
+     * @param post Post 엔티티
+     */
     public PostResponseDto(Post post) {
         this.id = post.getId();
         this.userId = post.getUser().getId();
