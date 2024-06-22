9c5e408dd08095f69d94a6be080f78537e4a565d
hgi4657 <hgi4657@naver.com>
2024-06-22 17:54:38 +0900

    feat: Make Password Encoded handling
    PasswordEncoder 주입
    빌더 패던을 사용해서 Dto 반환
    set 메서드 삭제로 인한 update 메서드 추가
    사용하지 않는 import 삭제

--- src/main/java/com/sparta/sixhundredbills/profile/service/ProfileService.java
+++ src/main/java/com/sparta/sixhundredbills/profile/service/ProfileService.java
@@ -9,6 +9,9 @@ import com.sparta.sixhundredbills.profile.dto.ProfileResponseDto;
 import com.sparta.sixhundredbills.profile.entity.PasswordList;
 import com.sparta.sixhundredbills.profile.repository.PasswordListRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +21,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProfileService {
 
+    private static final Logger log = LoggerFactory.getLogger(ProfileService.class);
     private final UserRepository userRepository;
     private final PasswordListRepository passwordListRepository;
+    private final PasswordEncoder passwordEncoder;
 
     // 각 사용자가 가질 수 있는 패스워드 리스트 최대 사이즈
     private static final int LIST_MAX_SIZE = 3;
@@ -35,7 +40,7 @@ public class ProfileService {
                 new UnauthorizedException("유효하지 않은 토큰입니다")
         );
         // 조회된 유저 정보를 프로필 Dto 에 필요한 내용만 담아서 반환
-        return new ProfileResponseDto(getUser);
+        return ProfileResponseDto.fromUser(getUser);
     }
 
     /**
@@ -52,7 +57,7 @@ public class ProfileService {
         );
 
         // 현재 비밀번호와 입력받은 비밀번호가 동일한지 확인
-        if (!getUser.getPassword().equals(profileRequestDto.getPassword())) {
+        if (!passwordEncoder.matches(profileRequestDto.getPassword(), getUser.getPassword())) {
             throw new InvalidEnteredException("현재 비밀번호와 일치하지 않습니다.");
         }
 
@@ -60,7 +65,7 @@ public class ProfileService {
         List<PasswordList> usePasswords = passwordListRepository.findByUserOrderByCreatedAtDesc(getUser);
         // 입력받은 새 비밀번호가 최근 사용했던 패스워드 목록에 존재하는지 확인
         for (PasswordList passwordList : usePasswords) {
-            if (passwordList.getPassword().equals(profileRequestDto.getNewPassword())) {
+            if (passwordEncoder.matches(profileRequestDto.getNewPassword(), passwordList.getPassword())) {
                 throw new InvalidEnteredException("새로운 비밀번호는 현재 비밀번호 및 최근 사용한 비밀번호와 달라야 합니다.");
             }
         }
@@ -70,21 +75,23 @@ public class ProfileService {
             PasswordList passwordList = usePasswords.get(usePasswords.size() - 1);
             passwordListRepository.delete(passwordList);
         }
+
+        // 인코딩된 새 패스워드 변수에 저장
+        String encodedPassword = passwordEncoder.encode(profileRequestDto.getNewPassword());
+
         // 3개 미만일 경우 패스워드 목록에 필요한 데이터 설정
         PasswordList newPasswordList = PasswordList.builder()
-                .password(profileRequestDto.getNewPassword())
+                .password(encodedPassword)
                 .user(getUser)
                 .build();
 
         // 패스워드 목록에 추가
         passwordListRepository.save(newPasswordList);
 
-        // 요청한 새로운 비밀번호를 반영
-        getUser.setPassword(profileRequestDto.getNewPassword());
+        // 수정 요청한 유저 정보 업데이트
+        getUser.updateProfile(profileRequestDto, encodedPassword);
 
-        // 수정된 유저 정보 DB 에 저장
-        userRepository.save(getUser);
         // 수정 완료된 유저 정보를 프로필 Dto 에 내용을 담아서 반환
-        return new ProfileResponseDto(getUser);
+        return ProfileResponseDto.fromUser(getUser);
     }
 }
