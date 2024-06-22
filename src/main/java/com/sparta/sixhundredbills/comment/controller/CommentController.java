5a136d5827b9644dd169071e6d0c21d717006363
윤일영 <162955567+1004102@users.noreply.github.com>
2024-06-23 05:49:19 +0900

    refactor : deleteComment Controller 메서드명 수정

--- src/main/java/com/sparta/sixhundredbills/comment/controller/CommentController.java
+++ src/main/java/com/sparta/sixhundredbills/comment/controller/CommentController.java
@@ -87,7 +87,7 @@ public class CommentController {
      * @return 삭제된 댓글의 응답 데이터
      */
     @DeleteMapping("/{postId}/comments/{commentId}")
-    public ResponseEntity<String> deleteComemnt(@PathVariable Long postId,
+    public ResponseEntity<String> deleteComment(@PathVariable Long postId,
                                                 @PathVariable Long commentId,
                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
         String responseDto = commentService.deleteComment(postId, commentId, userDetails);
