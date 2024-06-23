c49809d315e1ae5e4c40e7919203dcd7a92e3e36
gminnimk <tvt1245@naver.com>
2024-06-23 18:47:35 +0900

    feat: 회원 권한 부여 로직 구현.
    ADD
       - 사용자가 회원 가입 시 선택할 수 있도록 role 필드를 추가하여 회원 권한을 지정할 수 있게 함.

--- src/main/java/com/sparta/sixhundredbills/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/sixhundredbills/auth/dto/SignupRequestDto.java
@@ -1,6 +1,6 @@
 package com.sparta.sixhundredbills.auth.dto;
 
-
+import com.sparta.sixhundredbills.auth.entity.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,7 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-/*
+/**
  * 회원가입 요청을 위한 데이터 전송 객체 (DTO).
  * 각 필드는 유효성 검사를 포함하며, Lombok을 사용하여 getter와 builder 메소드를 자동 생성.
  */
@@ -47,10 +47,17 @@ public class SignupRequestDto {
     @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
 
+    /**
+     * 회원 권한
+     * - 기본값: USER
+     */
+    private Role role;
+
     @Builder
-    public SignupRequestDto(String email, String password, String name) {
+    public SignupRequestDto(String email, String password, String name, Role role) {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.role = role;
     }
 }
