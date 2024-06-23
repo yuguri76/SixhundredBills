af1455cc08ca1f9ac1612380e9cc1ef9fa243dd6
hgi4657 <hgi4657@naver.com>
2024-06-24 02:44:32 +0900

    fix: ProfileRequestDto Add Validation
    프로필 수정 validation 추가

--- src/main/java/com/sparta/sixhundredbills/profile/dto/ProfileRequestDto.java
+++ src/main/java/com/sparta/sixhundredbills/profile/dto/ProfileRequestDto.java
@@ -1,12 +1,26 @@
 package com.sparta.sixhundredbills.profile.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ProfileRequestDto {  // 요청
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$",
+            message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야합니다. ")
     private String password;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$",
+            message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야합니다. ")
     private String newPassword;
 }
