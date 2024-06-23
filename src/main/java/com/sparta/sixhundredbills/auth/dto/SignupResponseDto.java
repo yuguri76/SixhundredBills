0c4080089cabce103e29d0f325f69fff5072cf57
gminnimk <tvt1245@naver.com>
2024-06-23 18:50:53 +0900

    feat: 회원 권한 부여 로직 구현.
    ADD
       - role 필드는 User 엔티티의 Role 객체를 가져와서 설정.
        => 객체 생성 시 사용자의 권한 정보 함께 전달.

--- src/main/java/com/sparta/sixhundredbills/auth/dto/SignupResponseDto.java
+++ src/main/java/com/sparta/sixhundredbills/auth/dto/SignupResponseDto.java
@@ -1,7 +1,7 @@
 package com.sparta.sixhundredbills.auth.dto;
 
+import com.sparta.sixhundredbills.auth.entity.Role;
 import com.sparta.sixhundredbills.auth.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -12,14 +12,14 @@ import java.time.LocalDateTime;
  */
 
 @Getter
-@AllArgsConstructor
 public class SignupResponseDto {
 
     private String email;          // 사용자 아이디
-    private String name;              // 사용자 이름
+    private String name;           // 사용자 이름
     private LocalDateTime createdAt;  // 계정 생성 시간
     private LocalDateTime modifiedAt; // 계정 수정 시간
     private String userStatusEnum;    // 사용자 상태
+    private Role role;              // 사용자 권한
 
     /**
      * User 엔티티 객체로부터 데이터를 가져와서 SignupResponseDto 객체를 생성.
@@ -28,10 +28,10 @@ public class SignupResponseDto {
      */
     public SignupResponseDto(User user) {
         this.email = user.getEmail();        // User 엔티티로부터 사용자 아이디를 가져옴
-        this.name = user.getName();                // User 엔티티로부터 사용자 이름을 가져옴
+        this.name = user.getName();          // User 엔티티로부터 사용자 이름을 가져옴
         this.createdAt = user.getCreatedAt();      // User 엔티티로부터 계정 생성 시간을 가져옴
         this.modifiedAt = user.getModifiedAt();    // User 엔티티로부터 계정 수정 시간을 가져옴
         this.userStatusEnum = user.getUserStatus().getStatus(); // User 엔티티로부터 사용자 상태를 가져옴
+        this.role = Role.valueOf(user.getRole());         // User 엔티티로부터 사용자 권한을 가져옴
     }
 }
-
